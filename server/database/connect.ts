71d313736b5a457dafa666c3db0cd2feee8681d8
zacharytyhacz <zactyh@gmail.com>
2024-07-28 21:55:42 -0400

    Added output for when it's connecting to DB

--- server/database/connect.ts
+++ server/database/connect.ts
@@ -6,9 +6,11 @@ export const connectToMongo = async () => {
     const { MONGO_URI } = config()
 
     try {
+        console.log('Info: Connecting to MongoDB...')
         await mongoose.connect(MONGO_URI)
+        console.log('Success: Connected to MongoDB.')
     } catch (err) {
-        console.error(`Failed to connect to ${MONGO_URI}`, err)
+        console.error(`Error: Failed to connect to ${MONGO_URI}`, err)
         throw err
     }
 }
